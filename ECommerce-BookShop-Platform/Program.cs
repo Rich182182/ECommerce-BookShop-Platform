fd71fc8973cbabdac821b1d347e683665099407f
Rich182182 <120336828+Rich182182@users.noreply.github.com>
2026-06-16 23:37:11 +0300

    Update Program.cs

--- ECommerce-BookShop-Platform/Program.cs
+++ ECommerce-BookShop-Platform/Program.cs
@@ -11,7 +11,6 @@ using Rich.DataAccess.DbInitializer;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options=> 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -46,11 +45,9 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
